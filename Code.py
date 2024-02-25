b96a69e52b85f008da1885395eaec6cba630e698
Mahdi <99264632+mam127@users.noreply.github.com>
2024-02-25 17:30:27 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1353,7 +1353,7 @@ button:hover, a:hover {
 </head>
 <body>
 <div class="card">
-  <img src="https://media.licdn.com/dms/image/C4E03AQGYYhglXBeeSw/profile-displayphoto-shrink_800_800/0/1656601448671?e=1681344000&v=beta&t=UuVmSgL0uckt20iII2DHhdj9F_tBSKyR8GiD5kXi12A" alt="Mahdi Mohammad" style="width:100%; height:300px">
+  <img src="https://media.licdn.com/dms/image/C4E03AQGYYhglXBeeSw/profile-displayphoto-shrink_800_800/0/1656601450069?e=2147483647&v=beta&t=cHV2FkoiHiv19nIdDgDD06B3p5KSGF4AHDFYzfrc5tU" alt="Mahdi Mohammad" style="width:100%; height:300px">
   <h1 style="font-size: 25px; color:green"><b>Mahdi Mohammad</b></h1>
   <p style="color:black" class="title">Graduate<br>MSc. in Business Analytics (MSBA)</p>
   <div style="margin: 24px 0;">
